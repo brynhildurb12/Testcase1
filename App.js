8fe81b553df543df3ceca9a77dfbb1832b7d0583
Sara Ben <sara.benediktsdottir@bublar.com>
2017-08-22 22:20:55 +0200

    New background colour

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text>Welcome to the Swaaaaagers Frisbee App Fabbið!</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -16,7 +17,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2FD566',
     alignItems: 'center',
     justifyContent: 'center',
   },
